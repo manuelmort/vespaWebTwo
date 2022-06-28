7f183aba800b4b122be17a0ed43ec9be67d4ea97
Manuel Morteo <manuelmort@Manuels-MacBook-Pro.local>
2022-06-28 10:22:15 -0700

    adding a few nav changes

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -46,7 +46,7 @@ export default class NavigationBar extends React.Component {
                     <div class="hidden w-full md:block md:w-auto bg-transparent" id="mobile-menu">
                     <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium bg-transparent" >
                         <li class="bg-transparent">
-                            <a href="apprenticeship101" class="block py-2 pr-4 pl-3 text-white bg-transparent rounded md:bg-transparent  md:p-0 dark:text-white" aria-current="page">Apprenticeship</a>
+                            <a href="/apprenticeship101" class="block py-2 pr-4 pl-3 text-white bg-transparent rounded md:bg-transparent  md:p-0 dark:text-white" aria-current="page">Apprenticeship</a>
                         </li>
                         <li class="bg-transparent">
                             <a href="/tutorial" class="block py-2 pr-4 pl-3 text-gray-700 border-b bg-transparent border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">tutorial</a>
